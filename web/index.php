df7e9eae015108009f00a810aeeb98958bcf1341
Fabiano Santos <fabiano@fabianosantos.net>
2018-05-19 15:24:30 -0300

    Improve some changes.

--- web/index.php
+++ web/index.php
@@ -1,9 +1,15 @@
 <?php
-$uri = urldecode(trim(ltrim($_SERVER['REQUEST_URI'],'/')));
-if (!$uri) {
-  echo file_get_contents(__DIR__.'/homepage.html');
-  exit;
-}
-header('HTTP/1.1 301 Moved Permanently');
-header('Location: lbry://' . $uri);
-echo '<meta http-equiv="refresh" content="0;url=lbry://' . htmlspecialchars($uri, ENT_QUOTES, 'UTF-8') . '">';
+	$uri=trim($_SERVER['REQUEST_URI'],'/');
+	$uri=urldecode($uri);
+
+	if (!$uri)
+	{
+	  echo file_get_contents(__DIR__.'/homepage.html');
+	  exit;
+	}
+	 
+	header('HTTP/1.1 301 Moved Permanently');
+	header('Location: lbry://' . $uri);
+	echo '<meta http-equiv="refresh" content="0;url=lbry://' . htmlspecialchars($uri, ENT_QUOTES, 'UTF-8') . '">';
+
+?>
