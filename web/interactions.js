313a9b2bbb3e58047b8ccaf9182974fefc7ad159
Fabiano Santos <fabiano@fabianosantos.net>
2018-05-31 12:24:36 -0300

    web/style.css -> added some css and organize some. let wlink hidden. improve style. web/homepage.html -> added lbry protocol before input text. changed placeholder info according to @tzarebczan comment. web/interactions.js -> added wlink element to display onkeypress. removed encoding url to textContent.

--- web/interactions.js
+++ web/interactions.js
@@ -1,11 +1,22 @@
+window.onload=function(){
+
     const name = document.getElementById("name"),
-          link = document.getElementById("link");
+          link = document.getElementById("link"),
+	  wlink = document.getElementById("wlink");
 
     function handler() {
-      const url = "https://open.lbry.io/" + encodeURIComponent(name.value.trim());
-      link.href=url;
-      link.textContent=url;
+      const url = "https://open.lbry.io/";
+      link.href=url+encodeURIComponent(name.value.trim());
+      link.textContent=name.value.trim();      
     }
 
+    function presskey()
+    {
+      wlink.style.display="block";
+    }
+    
     name.addEventListener("input", handler);
+    name.addEventListener("input", presskey);
+    
     handler();
+}
