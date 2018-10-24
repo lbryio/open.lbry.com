d433da7f9687b0e63be8cd69e8e029865533fd4e
ポール ウェッブ <netopwibby@thenetwork.email>
2018-10-24 11:11:34 -0500

    Changes and fixes

--- web/index.php
+++ web/index.php
@@ -2,14 +2,13 @@
 	$uri=trim($_SERVER['REQUEST_URI'],'/');
 	$uri=urldecode($uri);
 
-	if (!$uri)
-	{
+	if (!$uri) {
 	  echo file_get_contents(__DIR__.'/homepage.html');
 	  exit;
 	}
-	 
+
+	echo file_get_contents(__DIR__.'/interstitial.html');
 	header('HTTP/1.1 301 Moved Permanently');
 	header('Location: lbry://' . $uri);
 	echo '<meta http-equiv="refresh" content="0;url=lbry://' . htmlspecialchars($uri, ENT_QUOTES, 'UTF-8') . '">';
-
 ?>
--- web/interactions.js
+++ web/interactions.js
@@ -1,22 +1,18 @@
-window.onload=function(){
+window.onload = function () {
+  const name = document.getElementById("name");
+  const link = document.getElementById("link");
+  const wlink = document.getElementById("wlink");
 
-    const name = document.getElementById("name"),
-          link = document.getElementById("link"),
-	  wlink = document.getElementById("wlink");
+  function handler() {
+    const url = "https://open.lbry.io/";
 
-    function handler() {
-      const url = "https://open.lbry.io/";
-      link.href=url+encodeURIComponent(name.value.trim());
-      link.textContent=name.value.trim();      
-    }
+    link.href = url + encodeURIComponent(name.value.trim());
+    link.textContent = name.value.trim();
 
-    function presskey()
-    {
-      wlink.style.display="block";
-    }
-    
-    name.addEventListener("input", handler);
-    name.addEventListener("input", presskey);
-    
-    handler();
+    if (name.value.length > 0) wlink.style.display = "block";
+    else wlink.style.display = "none";
+  }
+
+  name.addEventListener("input", handler);
+  handler();
 }
