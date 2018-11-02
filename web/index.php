d646af08270fb82bfcdd8e367566e027dc6d061f
ポール ウェッブ <netopwibby@thenetwork.email>
2018-11-02 12:53:51 -0500

    Fixes

--- web/index.php
+++ web/index.php
@@ -1,14 +1,3 @@
 <?php
-	$uri=trim($_SERVER['REQUEST_URI'],'/');
-	$uri=urldecode($uri);
-
-	if (!$uri) {
-	  echo file_get_contents(__DIR__.'/homepage.html');
-	  exit;
-	}
-
 	echo file_get_contents(__DIR__.'/interstitial.html');
-	header('HTTP/1.1 301 Moved Permanently');
-	header('Location: lbry://' . $uri);
-	echo '<meta http-equiv="refresh" content="0;url=lbry://' . htmlspecialchars($uri, ENT_QUOTES, 'UTF-8') . '">';
 ?>
