213a24b5655c057106deb537bb4ad95bd03c9ae3
Alex Grintsvayg <git@grin.io>
2018-11-05 09:19:29 -0500

    a few fixes

--- web/index.php
+++ web/index.php
@@ -1,3 +1,12 @@
 <?php
-	echo file_get_contents(__DIR__.'/interstitial.html');
-?>
+
+$uri = urldecode(trim($_SERVER['REQUEST_URI'], '/'));
+echo $uri ?
+  file_get_contents(__DIR__ . '/redirect.html') :
+  file_get_contents(__DIR__ . '/homepage.html');
+
+//
+//header('HTTP/1.1 301 Moved Permanently');
+//header('Location: lbry://' . $uri);
+//echo '<meta http-equiv="refresh" content="0;url=lbry://' . htmlspecialchars($uri, ENT_QUOTES, 'UTF-8') . '">';
+//
