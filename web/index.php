cddc379c98085b6a6571876760a9f11e7c761368
Niko <niko@lbry.com>
2019-12-02 19:24:20 +0100

    redirect all requests

--- web/index.php
+++ web/index.php
@@ -2,10 +2,10 @@
 
 $uri = urldecode(trim($_SERVER['REQUEST_URI'], '/'));
 
-echo $uri ?
-  file_get_contents(__DIR__ . '/redirect.html') :
-  file_get_contents(__DIR__ . '/homepage.html');
+//echo $uri ?
+//  file_get_contents(__DIR__ . '/redirect.html') :
+//  file_get_contents(__DIR__ . '/homepage.html');
 
-// header('HTTP/1.1 301 Moved Permanently');
-// header('Location: lbry://' . $uri);
-// echo '<meta http-equiv="refresh" content="0;url=lbry://' . htmlspecialchars($uri, ENT_QUOTES, 'UTF-8') . '">';
+header('HTTP/1.1 301 Moved Permanently');
+header('Location: https://lbry.tv/' . $uri);
+echo '<meta http-equiv="refresh" content="0;url=https://lbry.tv' . htmlspecialchars($uri, ENT_QUOTES, 'UTF-8') . '">';
